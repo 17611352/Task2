6c655687a98db5a31266136b3d693768df83e714
17611352 <56213056+17611352@users.noreply.github.com>
2019-10-06 22:34:36 +0200

    Added original task 2 files to repo location

---
+++ Task_2/Form1.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace Task_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.Start_Button = new System.Windows.Forms.Button();
+            this.Display_Group_Box = new System.Windows.Forms.GroupBox();
+            this.Test_Display_Label = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.Pause_Button = new System.Windows.Forms.Button();
+            this.Save_Game_Button = new System.Windows.Forms.Button();
+            this.Load_Save_Game = new System.Windows.Forms.Button();
+            this.Unit_Info_Display_RichTextBox = new System.Windows.Forms.RichTextBox();
+            this.Num_Of_Rounds = new System.Windows.Forms.Label();
+            this.Num_Of_Units_InGame = new System.Windows.Forms.Label();
+            this.Buildings_In_Game = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Start_Button
+            // 
+            this.Start_Button.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Start_Button.Location = new System.Drawing.Point(668, 44);
+            this.Start_Button.Name = "Start_Button";
+            this.Start_Button.Size = new System.Drawing.Size(86, 36);
+            this.Start_Button.TabIndex = 0;
+            this.Start_Button.Text = "Start";
+            this.Start_Button.UseVisualStyleBackColor = true;
+            this.Start_Button.Click += new System.EventHandler(this.Start_Button_Click);
+            // 
+            // Display_Group_Box
+            // 
+            this.Display_Group_Box.Location = new System.Drawing.Point(97, 77);
+            this.Display_Group_Box.Name = "Display_Group_Box";
+            this.Display_Group_Box.Size = new System.Drawing.Size(548, 516);
+            this.Display_Group_Box.TabIndex = 2;
+            this.Display_Group_Box.TabStop = false;
+            this.Display_Group_Box.Text = "BattleField";
+            // 
+            // Test_Display_Label
+            // 
+            this.Test_Display_Label.AutoSize = true;
+            this.Test_Display_Label.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Test_Display_Label.Location = new System.Drawing.Point(12, 44);
+            this.Test_Display_Label.Name = "Test_Display_Label";
+            this.Test_Display_Label.Size = new System.Drawing.Size(64, 25);
+            this.Test_Display_Label.TabIndex = 3;
+            this.Test_Display_Label.Text = "label1";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // Pause_Button
+            // 
+            this.Pause_Button.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Pause_Button.Location = new System.Drawing.Point(668, 86);
+            this.Pause_Button.Name = "Pause_Button";
+            this.Pause_Button.Size = new System.Drawing.Size(86, 36);
+            this.Pause_Button.TabIndex = 4;
+            this.Pause_Button.Text = "Pause";
+            this.Pause_Button.UseVisualStyleBackColor = true;
+            this.Pause_Button.Click += new System.EventHandler(this.Pause_Button_Click);
+            // 
+            // Save_Game_Button
+            // 
+            this.Save_Game_Button.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Save_Game_Button.Location = new System.Drawing.Point(816, 23);
+            this.Save_Game_Button.Name = "Save_Game_Button";
+            this.Save_Game_Button.Size = new System.Drawing.Size(86, 57);
+            this.Save_Game_Button.TabIndex = 5;
+            this.Save_Game_Button.Text = "Save Game";
+            this.Save_Game_Button.UseVisualStyleBackColor = true;
+            // 
+            // Load_Save_Game
+            // 
+            this.Load_Save_Game.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Load_Save_Game.Location = new System.Drawing.Point(816, 86);
+            this.Load_Save_Game.Name = "Load_Save_Game";
+            this.Load_Save_Game.Size = new System.Drawing.Size(86, 70);
+            this.Load_Save_Game.TabIndex = 6;
+            this.Load_Save_Game.Text = "Load Save Game";
+            this.Load_Save_Game.UseVisualStyleBackColor = true;
+            // 
+            // Unit_Info_Display_RichTextBox
+            // 
+            this.Unit_Info_Display_RichTextBox.Location = new System.Drawing.Point(672, 202);
+            this.Unit_Info_Display_RichTextBox.Name = "Unit_Info_Display_RichTextBox";
+            this.Unit_Info_Display_RichTextBox.Size = new System.Drawing.Size(230, 325);
+            this.Unit_Info_Display_RichTextBox.TabIndex = 7;
+            this.Unit_Info_Display_RichTextBox.Text = "";
+            // 
+            // Num_Of_Rounds
+            // 
+            this.Num_Of_Rounds.AutoSize = true;
+            this.Num_Of_Rounds.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Num_Of_Rounds.Location = new System.Drawing.Point(278, 37);
+            this.Num_Of_Rounds.Name = "Num_Of_Rounds";
+            this.Num_Of_Rounds.Size = new System.Drawing.Size(146, 25);
+            this.Num_Of_Rounds.TabIndex = 8;
+            this.Num_Of_Rounds.Text = "Num of Rounds";
+            // 
+            // Num_Of_Units_InGame
+            // 
+            this.Num_Of_Units_InGame.AutoSize = true;
+            this.Num_Of_Units_InGame.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Num_Of_Units_InGame.Location = new System.Drawing.Point(92, 9);
+            this.Num_Of_Units_InGame.Name = "Num_Of_Units_InGame";
+            this.Num_Of_Units_InGame.Size = new System.Drawing.Size(150, 25);
+            this.Num_Of_Units_InGame.TabIndex = 10;
+            this.Num_Of_Units_InGame.Text = "Units in Game : ";
+            // 
+            // Buildings_In_Game
+            // 
+            this.Buildings_In_Game.AutoSize = true;
+            this.Buildings_In_Game.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Buildings_In_Game.Location = new System.Drawing.Point(465, 9);
+            this.Buildings_In_Game.Name = "Buildings_In_Game";
+            this.Buildings_In_Game.Size = new System.Drawing.Size(180, 25);
+            this.Buildings_In_Game.TabIndex = 11;
+            this.Buildings_In_Game.Text = "Buildings in Game :";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1145, 605);
+            this.Controls.Add(this.Buildings_In_Game);
+            this.Controls.Add(this.Num_Of_Units_InGame);
+            this.Controls.Add(this.Num_Of_Rounds);
+            this.Controls.Add(this.Unit_Info_Display_RichTextBox);
+            this.Controls.Add(this.Load_Save_Game);
+            this.Controls.Add(this.Save_Game_Button);
+            this.Controls.Add(this.Pause_Button);
+            this.Controls.Add(this.Test_Display_Label);
+            this.Controls.Add(this.Display_Group_Box);
+            this.Controls.Add(this.Start_Button);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Start_Button;
+        private System.Windows.Forms.GroupBox Display_Group_Box;
+        private System.Windows.Forms.Label Test_Display_Label;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button Pause_Button;
+        private System.Windows.Forms.Button Save_Game_Button;
+        private System.Windows.Forms.Button Load_Save_Game;
+        private System.Windows.Forms.RichTextBox Unit_Info_Display_RichTextBox;
+        private System.Windows.Forms.Label Num_Of_Rounds;
+        private System.Windows.Forms.Label Num_Of_Units_InGame;
+        private System.Windows.Forms.Label Buildings_In_Game;
+    }
+}
+
---
+++ Task_2/Map.cs
@@ -0,0 +1,358 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace Task_3
+{
+    public class Map
+    {
+        int roundCheck = 0;
+
+        GameEngine gE;
+        Random r = new Random();
+        public int test = 0;
+        int xMapSize = 20;
+        int yMapSize = 20;
+
+        List<Unit> units;
+        private List<Building> buildings = new List<Building>();
+        int buildingRd = 0;
+
+        public List<Unit> Units
+        {
+            get { return units; }
+            set { units = value; }
+        }
+
+        public List<Building> Buildings
+        {
+            get { return buildings; }
+            set { buildings = value; }
+        }
+
+        public Map()
+        {
+            units = new List<Unit>();
+            //SpawnUnitAtFactory();
+            GenerateUnits(5);
+            //Buildings = new List<Building>();
+        }
+
+        int[] listOfPositions = new int[100];
+        int nearestDistance;
+        int positionInList;
+
+        public void GenerateUnits(int numOfBuildingsToSpawn)//Gets Called Only Once
+        {
+            //buildings = new List<Building>();
+        
+            //Spawns Buildings
+            for (int b = 0; b < numOfBuildingsToSpawn; b++)
+            {
+                buildingRd = r.Next(0, 5);
+
+                if(buildingRd >= 3)
+                {
+                    ResourceBuilding rb = new ResourceBuilding(r.Next(0, 20),
+                                                            r.Next(0, 20),
+                                                            200,
+                                                            r.Next(0, 5),
+                                                            "R");
+
+                    buildings.Add(rb);
+                }
+                else
+                {
+                    FactoryBuilding fb = new FactoryBuilding(r.Next(0, 20),
+                                                            r.Next(0, 20),
+                                                            100,
+                                                            r.Next(0, 5),
+                                                            "F");
+
+                    buildings.Add(fb);
+                }
+
+
+            }
+
+
+            //Generates Melee Units
+            //for (int i = 0; i < 20; i++)
+            //{
+            //    Melee_Unit mU = new Melee_Unit();
+            //    mU.xPos = r.Next(2, xMapSize);
+            //    mU.yPos = r.Next(2, yMapSize);
+            //    mU.symbol = "{:}";
+            //    mU.health = 200;
+
+            //    units.Add(mU);
+            //}
+
+            ////Generates Ranged Units
+            //for (int i = 0; i < 20; i++)
+            //{
+            //    Ranged_Unit rU = new Ranged_Unit();
+            //    rU.xPos = r.Next(0, xMapSize);
+            //    rU.yPos = r.Next(0, yMapSize);
+            //    rU.symbol = "[!]";
+            //    rU.health = 100;
+
+            //    units.Add(rU);
+            //}
+
+        }
+
+
+        public void SpawnUnitAtFactory()
+        {
+            //for(int i = 0; i < 20; i++)
+            //{
+            //    Melee_Unit m = new Melee_Unit(r.Next(0, 20), r.Next(0, 20) + 5, 200, 1, 20, 1, "{:}", "Red");
+            //    units.Add(m);
+            //}
+
+            //for (int i = 0; i < 20; i++)
+            //{
+            //    Ranged_Unit rU = new Ranged_Unit(r.Next(0, 20), r.Next(0, 20) + 5, 100, 1, 20, 5, "[!]", "Blue");
+            //    units.Add(rU);
+            //}
+
+
+
+            foreach (Building bd in buildings)
+            {
+
+                if (bd is FactoryBuilding)
+                {
+                    if (bd.symbol == "F" && bd.team < 3)
+                    {
+                            Ranged_Unit rU = new Ranged_Unit(bd.xPosition, bd.yPosition + 5, 100, 1, 20, 5, "[!]", "Red");
+                            //rU.Xpos = bd.xPosition;
+                            //rU.Ypos = bd.yPosition + 5;
+                            //rU.Symbol = "[!]";
+                            //rU.Health = 100;
+                            //rU.Attack = 20;
+                            //rU.AttackRange = 5;
+                            //rU.IsAttacking = false;
+                            //rU.IsDead = false;
+                            //rU.Team = "Red";
+
+                            units.Add(rU);
+                       
+                            Melee_Unit mU = new Melee_Unit(bd.xPosition, bd.yPosition + 5, 200, 1, 20, 1, "{:}", "Red");
+                            //mU.Xpos = bd.xPosition;
+                            //mU.Ypos = bd.xPosition + 5;
+                            //mU.Symbol = "{:}";
+                            //mU.Health = 200;
+                            //mU.Attack = 20;
+                            //mU.AttackRange = 1;
+                            //mU.IsAttacking = false;
+                            //mU.IsDead = false;
+                            //mU.Team = "Red";
+
+                            units.Add(mU);
+                        
+
+                    }
+                    else if (bd.symbol == "F" && bd.team >= 3)
+                    {
+                      
+                            Ranged_Unit rU = new Ranged_Unit(bd.xPosition, bd.yPosition + 5, 100, 1, 20, 5, "[!]", "Red");
+                            //rU.Xpos = bd.xPosition;
+                            //rU.Ypos = bd.yPosition + 5;
+                            //rU.Symbol = "[!]";
+                            //rU.Health = 100;
+                            //rU.Team = "Blue";
+
+                            units.Add(rU);
+                      
+                            Melee_Unit mU = new Melee_Unit(bd.xPosition, bd.yPosition + 5, 200, 1, 20, 1, "{:}", "Blue");
+                            //mU.Xpos = bd.xPosition;
+                            //mU.Ypos = bd.xPosition + 5;
+                            //mU.Symbol = "{:}";
+                            //mU.Health = 200;
+                            //mU.Team = "Blue";
+
+                            units.Add(mU);
+                        
+
+                    }
+                }
+
+            }
+
+        }
+
+
+        public void DisplayUnits(GroupBox groupBox)
+        {
+            //Num_Of_Rounds.Text = "Round : " + gE.round.ToString();
+
+            groupBox.Controls.Clear();
+
+            foreach (Unit u in units)
+            {
+                Button newLabel = new Button();
+
+                if (u is Melee_Unit)
+                {
+                    Melee_Unit mu = (Melee_Unit)u;
+                    newLabel.Width = 25;
+                    newLabel.Height = 25;
+                    newLabel.Location = new Point(mu.Xpos * 20, mu.Ypos * 15);
+                    newLabel.Text = mu.Symbol;
+                    if (mu.Team == "Blue")
+                    {
+                        newLabel.ForeColor = Color.Blue;
+                    }
+                    else
+                    {
+                        newLabel.ForeColor = Color.Red;
+                    }
+                }
+                else if(u is Ranged_Unit)
+                {
+                    Ranged_Unit ru = (Ranged_Unit)u;
+                    newLabel.Width = 25;
+                    newLabel.Height = 25;
+                    newLabel.Location = new Point(ru.Xpos * 20, ru.Ypos * 15);
+                    newLabel.Text = ru.Symbol;
+                    if (ru.Team == "Blue")
+                    {
+                        newLabel.ForeColor = Color.Blue;
+                    }
+                    else
+                    {
+                        newLabel.ForeColor = Color.Red;
+                    }
+                }
+
+                groupBox.Controls.Add(newLabel);
+
+            }
+            
+
+
+            foreach (Building bd in buildings)
+            {
+                Button newLabel = new Button();
+                newLabel.Width = 30;
+                newLabel.Height = 30;
+                newLabel.Location = new Point(bd.xPosition * 20, bd.yPosition * 15);
+                newLabel.Text = bd.symbol;
+                if (bd.team >= 3)
+                {
+                    newLabel.ForeColor = Color.Blue;
+                }
+                else
+                {
+                    newLabel.ForeColor = Color.Red;
+                }
+
+                groupBox.Controls.Add(newLabel);
+            }
+
+            //test = groupBox.Controls.Count;
+        }
+
+
+        public void UpdateUnitMovement()
+        {
+            //First Checks For Closest Unit
+
+            //foreach (Unit unit in units)//This finds the closest unit to "this" unit
+            //{
+            //    //unit.Move();
+            //    //unit.ClosestUnit();
+
+            //    for (int i = 0; i < units.Count; i++)
+            //    {
+            //        if(unit.team != units.ElementAt(i).team)
+            //        {
+            //            double xSqr = Math.Pow(unit.xPos - units.ElementAt(i).xPos, 2);
+            //            double ySqr = Math.Pow(unit.yPos - units.ElementAt(i).yPos, 2);
+            //            double distance = Math.Round(Math.Sqrt(xSqr + ySqr));
+
+            //            listOfPositions[i] = Convert.ToInt32(distance);
+            //        }
+
+            //    }
+
+
+            //    if (listOfPositions.Min() == 0)
+            //    {
+            //        for (int u = 0; u < listOfPositions.GetLength(0); u++)
+            //        {
+            //            if (listOfPositions[u] == 0)
+            //            {
+            //                listOfPositions[u] = 1000;
+            //            }
+            //        }
+
+            //        nearestDistance = listOfPositions.Min();
+
+
+            //    }
+            //    else if (listOfPositions.Min() != 0)
+            //    {
+            //        nearestDistance = listOfPositions.Min();
+            //    }
+
+
+
+
+            //    for (int b = 0; b < listOfPositions.GetLength(0); b++)
+            //    {
+            //        if (listOfPositions[b] == nearestDistance)
+            //        {
+            //            positionInList = b;
+            //        }
+            //    }
+
+            //    int newXPos;
+            //    int newYPos;
+
+            //    newXPos = units.ElementAt(positionInList).xPos;
+            //    newYPos = units.ElementAt(positionInList).yPos;
+
+            //    double xNew = newXPos - unit.xPos;
+            //    double yNew = newYPos - unit.yPos;
+
+            //    int subXPos = 0;
+            //    int subYPos = 0;
+
+            //    if (xNew < 0)
+            //    {
+            //        //unit.xPos += -1;
+            //        subXPos += -1;
+            //    }
+            //    else if (xNew > 0)
+            //    {
+            //        //unit.xPos += 1;
+            //        subXPos += 1;
+            //    }
+
+
+            //    if (yNew < 0)
+            //    {
+            //        //unit.yPos += -1;
+            //        subYPos += -1;
+            //    }
+            //    else if (yNew > 0)
+            //    {
+            //        subYPos += 1;
+            //    }
+
+            //    unit.ClosestUnit(subXPos, subYPos);
+
+            //}
+
+        }
+
+
+    }
+}
